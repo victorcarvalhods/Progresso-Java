af7591c947ecfc95a6c6095227f2061b5dcc4ded
Victor Carvalho <victor1carvalho2002.vc@gmail.com>
2022-04-26 18:35:49 -0300

    Criado as bases da classe

---
+++ Excepetions 1/src/model/entities/Reserva.java
@@ -0,0 +1,72 @@
+package model.entities;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.concurrent.TimeUnit;
+
+public class Reserva {
+	private Integer numeroQuarto;
+	private Date entrada;
+	private Date saida;
+	
+	private static SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+	
+	public Reserva() {
+		
+	}
+
+	public Reserva(Integer numeroQuarto, Date entrada, Date saida) {
+		this.numeroQuarto = numeroQuarto;
+		this.entrada = entrada;
+		this.saida = saida;
+	}
+	
+	public long duracao() {
+		long diff = saida.getTime() - entrada.getTime();
+		
+		return TimeUnit.DAYS.convert(diff, TimeUnit.MILLISECONDS);
+	}
+	
+	public void atualizarDatas(Date entrada, Date saida) {
+		this.entrada = entrada;
+		this.saida = saida;
+	}
+
+	public Integer getNumeroQuarto() {
+		return numeroQuarto;
+	}
+
+	public void setNumeroQuarto(Integer numeroQuarto) {
+		this.numeroQuarto = numeroQuarto;
+	}
+
+	public Date getEntrada() {
+		return entrada;
+	}
+
+	public void setEntrada(Date entrada) {
+		this.entrada = entrada;
+	}
+
+	public Date getSaida() {
+		return saida;
+	}
+
+	public void setSaida(Date saida) {
+		this.saida = saida;
+	}
+	
+	@Override
+	public String toString() {
+		return "Quarto" + numeroQuarto 
+				+ ", entrada: " 
+				+ sdf.format(entrada) 
+				+ ", saída: " 
+				+ sdf.format(saida) 
+				+ ", " + duracao() + " noites";
+	}
+	
+	
+	
+}
